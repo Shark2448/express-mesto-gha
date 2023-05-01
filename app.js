cf328b1d3830c3c358941f3f74b7682ea1e65833
Alex Zverev <alexzverev2448@gmail.com>
2023-05-01 11:25:46 +0300

    logs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 
 const handleErrors = require('./middlewares/handleErrors');
 const router = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -14,7 +15,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
